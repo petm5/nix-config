b1f7baf8d17e973446b2d3cbc76c7410f37ffd5f
Peter Marshall <petms@proton.me>
2024-03-18 16:36:02 -0400

    Fix surface audio

--- hardware/microsoft/surface-pro-9-intel/audio/package.nix
+++ hardware/microsoft/surface-pro-9-intel/audio/package.nix
@@ -14,14 +14,14 @@ stdenv.mkDerivation {
     owner = "peter-marshall5";
     repo = "surface-audio";
     rev = "main";
-    hash = "sha256-flIW2hkvRwRArVXeLFm8mxjCaiUFBOJSEddQFVW5otM";
+    hash = "sha256-/4+GjIqZ+sI1kjhtez30PmRF5TMUpqrgx1DUDNTLvnY=";
   };
 
   installPhase = ''
     mkdir -p $out/share/wireplumber/policy.lua.d
     install 85-surface-policy.lua $out/share/wireplumber/policy.lua.d
-    mkdir -p $out/share/surface-audio
-    install devices/${device}/graph.json $out/share/surface-audio
-    install devices/${device}/impulse.wav $out/share/surface-audio
+    mkdir -p $out/share/wireplumber/surface-audio
+    install devices/${device}/graph.json $out/share/wireplumber/surface-audio
+    install devices/${device}/impulse.wav $out/share/wireplumber/surface-audio
   '';
 }

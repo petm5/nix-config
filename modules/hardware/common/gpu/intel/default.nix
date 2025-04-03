b0a05919f754578591b84e33ebb65326ec1b7ecb
Peter Marshall <petms@proton.me>
2025-04-03 13:43:44 -0400

    Remove unneeded Intel GPU parameters

--- modules/hardware/common/gpu/intel/default.nix
+++ modules/hardware/common/gpu/intel/default.nix
@@ -2,11 +2,6 @@
 
 {
 
-  boot.extraModprobeConfig = ''
-    options i915 enable_dc=4 disable_power_well=1 enable_fbc=1 enable_psr=2
-    options xe enable_dc=4 disable_power_well=1 enable_fbc=1 enable_psr=2
-  '';
-
   environment.variables = {
     VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
     VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";

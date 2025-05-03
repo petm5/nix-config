e7bdba662f6cce158b94d8ad1d447e854738a166
Peter Marshall <petms@proton.me>
2025-05-03 17:10:22 -0400

    SP9: Update DSP

--- modules/hardware/surface-pro-9-intel/audio.nix
+++ modules/hardware/surface-pro-9-intel/audio.nix
@@ -20,8 +20,8 @@
                       name = "bankstown"
                       control = {
                           bypass = 0
-                          amt = 0.8
-                          sat_third = 2.1
+                          amt = 1.6
+                          sat_third = 2.8
                           blend = 1
                           ceil = 200.0
                           floor = 20.0
@@ -78,7 +78,7 @@
                             rt_0 = 100
                             kn_0 = 0.125
                             cbe_1 = 1
-                            sf_1 = 380.0
+                            sf_1 = 200.0
                             ce_1 = 0
                             cbe_2 = 0
                             ce_2 = 0

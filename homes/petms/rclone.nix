905d4c466d42d27ae843806bde87489667503680
Peter Marshall <petms@proton.me>
2025-09-14 16:06:57 -0400

    add module to sync keepassxc with google drive

---
+++ homes/petms/rclone.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }: {
+
+  systemd.user.timers."rclone" = {
+    Unit = {
+      Description = "RClone Sync Timer";
+    };
+    Timer = {
+      Unit = "rclone.service";
+      OnBootSec = "2m";
+      OnUnitInactiveSec = "2m";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.paths."rclone" = {
+    Path = {
+      PathChanged = "%h/gdrive/keepassxc";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.services."rclone" = {
+    Unit = {
+      Description = "RClone Sync";
+    };
+    Service = {
+      ExecStart = "${pkgs.rclone}/bin/rclone bisync \"gdrive:/keepassxc\" \"gdrive/keepassxc\" --force --conflict-resolve older --conflict-loser num --resilient --quiet";
+      Restart = "on-failure";
+    };
+  };
+
+}

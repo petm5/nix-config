784dc542c94db2285d4098357049a0e845b6953b
Peter Marshall <petms@proton.me>
2025-12-27 18:07:57 -0500

    sp9: prioritize out-of-tree gpe driver

--- modules/hardware/surface-pro-9-intel/default.nix
+++ modules/hardware/surface-pro-9-intel/default.nix
@@ -9,16 +9,8 @@
 
   boot.extraModulePackages = [
     (pkgs.callPackage ../../../pkgs/ithc-linux/latest.nix { inherit (config.boot.kernelPackages) kernel kernelModuleMakeFlags; })
+    (lib.hiPrio (config.boot.kernelPackages.callPackage ../../../pkgs/surface-gpe {}))
   ];
-
-  systemd.services.surface-gpe = {
-    script = ''
-      ${pkgs.kmod}/bin/insmod ${(config.boot.kernelPackages.callPackage ../../../pkgs/surface-gpe {})}/lib/modules/*/updates/surface_gpe.ko*
-    '';
-    wantedBy = [ "default.target" ];
-    restartIfChanged = false;
-    serviceConfig.RemainAfterExit = true;
-  };
 
   boot.initrd.kernelModules = [ "nvme" "xhci_pci" "hid_generic" "atkbd" "surface_aggregator" "surface_aggregator_registry" "surface_aggregator_hub" "surface_hid_core" "8250_dw" "surface_hid" "intel_lpss" "intel_lpss_pci" "pinctrl_tigerlake" "usbhid" "ithc" ];
 

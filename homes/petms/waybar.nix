6962ba48221ba77226e61b7390b9bd738c039adf
Peter Marshall <petms@proton.me>
2025-12-29 20:08:57 -0500

    waybar: add mpris indicator

--- homes/petms/waybar.nix
+++ homes/petms/waybar.nix
@@ -1,7 +1,11 @@
-{
+{ pkgs, ...}: {
 
   programs.waybar.enable = true;
   programs.waybar.systemd.enable = true;
+
+  programs.waybar.package = pkgs.waybar.overrideAttrs {
+   withMediaPlayer = true;
+  };
 
   xdg.configFile."waybar/config".source = ./dotfiles/waybar/config;
   xdg.configFile."waybar/style.css".source = ./dotfiles/waybar/style.css;

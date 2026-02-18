8ddd9895e2be1c23462ebccc0f10e35167ae14b2
Peter Marshall <petms@proton.me>
2026-02-18 13:16:45 -0500

    fix binary cache url

--- modules/binary-cache.nix
+++ modules/binary-cache.nix
@@ -1,6 +1,6 @@
 {
   substituters = [
-    "binarycache.petermarshall.ca"
+    "http://binarycache.petermarshall.ca"
   ];
   trusted-public-keys = [
     "binarycache.petermarshall.ca:xmkaNcypcMm5i/8WxvNUtnuMND2bOoJYu1aUii9Eyao="

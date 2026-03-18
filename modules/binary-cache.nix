db11527c554ce2fd6143fb1a35ec9b86740dadcb
Peter Marshall <petms@proton.me>
2026-03-18 06:17:00 +0000

    binary-cache: add github signing key

--- modules/binary-cache.nix
+++ modules/binary-cache.nix
@@ -4,5 +4,6 @@
   ];
   trusted-public-keys = [
     "binarycache.petermarshall.ca:xmkaNcypcMm5i/8WxvNUtnuMND2bOoJYu1aUii9Eyao="
+    "binarycache.petermarshall.ca-gh:5A6yxY0hRi/qNxygl6IU+1z6NkmLZfdykUTWLciNj/g="
   ];
 }

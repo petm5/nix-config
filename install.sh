b47ad44351a9c391b1a64a9b1491085539e3843f
Peter Marshall <petms@proton.me>
2026-04-13 16:41:49 +0000

    install.sh: fix nix install failure causing lockout

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -e
+
 [ -d /nix ] || sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 nix run github:petm5/nix-config#homeConfigurations."vscode".activationPackage
 
 cat <<EOF >> ~/.bashrc
-if [ -d "$HOME/.nix-profile" ]; then
-    . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-fi
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
+. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 export COLORTERM=truecolor
 exec nu
 EOF

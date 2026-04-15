b95c1386d6805f9e10a825edbaf411965d336bca
Peter Marshall <petms@proton.me>
2026-04-15 01:11:40 +0000

    install.sh: add more checks

--- install.sh
+++ install.sh
@@ -6,23 +6,27 @@ TARGET_USER="nix-profile"
 TARGET_HOME="/home/$TARGET_USER"
 LOCKFILE="$HOME/.nix-setup-in-progress"
 
+[ $UID == 0 ] && echo "This script does not support running as root." && exit 1
+
 touch "$LOCKFILE"
 
 # Add command to enter the Nix-managed profile
-cat <<EOF >> "$HOME/.bashrc"
-enter-nix() {
-  if [ -e $LOCKFILE ]; then
-    echo "Waiting for Home Manager setup to finish (Ctrl+C to abort)..."
-    while [ -e $LOCKFILE ]; do
-      sleep 1 || return 1
-    done
-  fi
-  sudo su nix-profile -- $TARGET_HOME/.activate-nix
-}
+cat <<EOF | sudo tee /bin/enter-nix >/dev/null
+#!/bin/sh
+if [ -e $LOCKFILE ]; then
+  echo "Waiting for Home Manager setup to finish (Ctrl+C to abort)..."
+  while [ -e $LOCKFILE ]; do
+    sleep 1 || return 1
+  done
+fi
+sudo su nix-profile -- $TARGET_HOME/.activate-nix
 EOF
+sudo chmod +x /bin/enter-nix
 
 # Create a fresh login name with the same UID
-sudo useradd -p "!" -o -u "$UID" -m -d "$TARGET_HOME" "$TARGET_USER"
+if ! id -u "$TARGET_USER" 2>/dev/null; then
+  sudo useradd -p "!" -o -u "$UID" -m -d "$TARGET_HOME" "$TARGET_USER"
+fi
 
 # Switch to the new login name
 export USER=$TARGET_USER
@@ -38,7 +42,7 @@ fi
 nix run github:petm5/nix-config#homeConfigurations."vscode".activationPackage --extra-experimental-features "nix-command flakes" --accept-flake-config
 
 # Activate shell on login
-cat <<EOF >> "$HOME/.activate-nix"
+cat <<EOF > "$HOME/.activate-nix"
 #!/bin/sh
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"

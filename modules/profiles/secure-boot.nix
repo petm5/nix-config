49cfc247eded6313245adb18f0545ab703094f89
Peter Marshall <petms@proton.me>
2026-05-11 19:32:16 -0400

    profiles/secure-boot: enable measured boot

--- modules/profiles/secure-boot.nix
+++ modules/profiles/secure-boot.nix
@@ -14,26 +14,22 @@
       enable = true;
       pkiBundle = "/etc/secureboot";
       settings.reboot-for-bitlocker = true;
+      measuredBoot = {
+        enable = true;
+        pcrs = [
+          0
+          1
+          2
+          3
+          4
+        ];
+        staticMeasurements = {
+          "600-returning-from-efi-application".source = pkgs.writeText "600-returning-from-efi-application.pcrlock" ''
+            {"records":[{"pcr":4,"digests":[{"hashAlg":"sha256","digest":"7044f06303e54fa96c3fcd1a0f11047c03d209074470b1fd60460c9f007e28a6"}]},{"pcr":4,"digests":[{"hashAlg":"sha256","digest":"3d6772b4f84ed47595d72a2c4c5ffd15f5bb72c7507fe26f2aaee2c69d5633ba"}]}]}
+           '';
+         };
+      };
+      configurationLimit = 8;
     };
 
-   boot.initrd.systemd.storePaths = [
-     "${config.systemd.package}/lib/systemd/systemd-pcrextend"
-     "${pkgs.tpm2-tss}/lib"
-   ];
-
-   boot.initrd.systemd.additionalUpstreamUnits = [
-     "systemd-pcrphase-initrd.service"
-   ];
-
-   systemd.additionalUpstreamSystemUnits = [
-      "systemd-pcrphase.service"
-      "systemd-pcrphase-sysinit.service"
-   ];
-
-   environment.etc = {
-      systemd-pcrlock-builtin = {
-         target = "pcrlock.d";
-         source = "${config.systemd.package}/lib/pcrlock.d";
-      };
-   };
 }

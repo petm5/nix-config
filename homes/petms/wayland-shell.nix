faa0a404878d7c013944750a8c0facc2ff3dfb33
Peter Marshall <pm@petermarshall.ca>
2026-07-21 10:49:32 -0400

    home: remove waybar

--- homes/petms/wayland-shell.nix
+++ homes/petms/wayland-shell.nix
@@ -7,16 +7,6 @@
     settings.default-timeout = 15000;
   };
 
-  # programs.waybar.enable = true;
-  programs.waybar.systemd.enable = true;
-
-  programs.waybar.package = pkgs.waybar.overrideAttrs {
-   withMediaPlayer = true;
-  };
-
-  xdg.configFile."waybar/config".source = ./dotfiles/waybar/config;
-  xdg.configFile."waybar/style.css".source = ./dotfiles/waybar/style.css;
-
   programs.quickshell.enable = true;
   programs.quickshell.systemd.enable = true;
 
